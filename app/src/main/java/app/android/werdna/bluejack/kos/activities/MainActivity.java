374d4ba924ba004e6c774daafd54685dedd98670
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-03-23 17:42:41 +0700

    Add radio button to register screen

--- app/src/main/java/app/android/werdna/bluejack/kos/activities/MainActivity.java
+++ app/src/main/java/app/android/werdna/bluejack/kos/activities/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import app.android.werdna.bluejack.kos.R;
 
